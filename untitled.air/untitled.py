9382b3985cd77c3cc5299c9c95f313b32999ff3d
Leopzb <502342650@qq.com>
2022-05-29 10:46:34 +0800

    Initial commit

---
+++ untitled.air/untitled.py
@@ -0,0 +1,54 @@
+# -*- encoding=utf8 -*-
+__author__ = "Leopzb"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+
+
+
+
+
+i=1
+
+while 1:
+#     touch(Template(r"tpl1644908710054.png", record_pos=(0.001, 0.589), resolution=(405, 753)))  
+    
+    sleep(2)
+    touch(Template(r"tpl1644898279925.png", record_pos=(-0.009, 0.148), resolution=(405, 752)))
+    sleep(2)
+    
+    touch(Template(r"tpl1645083488303.png", record_pos=(-0.13, 0.686), resolution=(473, 873)))
+
+    touch(Template(r"tpl1645082846782.png", record_pos=(0.2, 0.212), resolution=(1080, 1920)))
+    sleep(5)
+    touch(Template(r"tpl1645083498823.png", record_pos=(0.109, 0.686), resolution=(473, 873)))
+
+    
+    touch(Template(r"tpl1645083410880.png", record_pos=(0.105, 0.263), resolution=(473, 873)))
+    touch(Template(r"tpl1645083507247.png", record_pos=(0.344, 0.682), resolution=(473, 873)))
+    touch(Template(r"tpl1645083513614.png", record_pos=(0.145, 0.251), resolution=(473, 873)))
+    sleep (60)
+    touch(Template(r"tpl1644898356493.png", record_pos=(0.199, 0.679), resolution=(405, 752)))
+    sleep(10)
+
+    
+# 控件单击
+# poco("challenage").click()
+
+# # 控件长按
+# poco('star_single').long_click()
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 抽卡.air/抽卡.py
@@ -0,0 +1,13 @@
+# -*- encoding=utf8 -*-
+__author__ = "Leopzb"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+
+while 1:
+    touch(Template(r"tpl1645068184010.png", record_pos=(-0.179, 0.636), resolution=(1080, 1920)))
+    sleep(4)
+
+
