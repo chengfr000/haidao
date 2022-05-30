afecacfeab50f96bc3464ed91180b182be0ec808
Leopzb <502342650@qq.com>
2022-05-30 17:44:32 +0800

    完成测试报告编写

--- 抽卡.air/抽卡.py
+++ 抽卡.air/抽卡.py
@@ -3,11 +3,49 @@ __author__ = "Leopzb"
 
 from airtest.core.api import *
 
-auto_setup(__file__)
+auto_setup(__file__,logdir=True)
+
+from airtest.report.report import LogToHtml, simple_report
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+
+# 自动配置运行环境，如果当前没有连接设备，默认尝试连接Android设备
+auto_setup(__file__,logdir=True)
+
+poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
+# 图片地址可以填写绝对路径
+touch(Template(r"D:\Airtest\haidao\抽卡.air\tpl1653811110876.png"))
+
+#sleep(5.0)
+#poco(text="我的").click()
+sleep(5.0)
+
+#poco(text="任务中心").click()
+
+#poco(text="开始游戏").click()
+
+# 测试报告
+# rp = LogToHtml(script_root=r"D:\Airtest\haidao\抽卡.air",log_root=r"D:\Airtest\haidao\log", export_dir=r'D:\Airtest\export',logfile=r'D:\Airtest\haidao\log\log.txt', lang='zh',  plugins=["poco.utils.airtest.report"])
+# rp.report(output_file=r"test.html")
 
 
-while 1:
-    touch(Template(r"tpl1645068184010.png", record_pos=(-0.179, 0.636), resolution=(1080, 1920)))
-    sleep(4)
+#打印测试报告
+simple_report(__file__,logpath=True,output=r"D:\Airtest\export\log.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
